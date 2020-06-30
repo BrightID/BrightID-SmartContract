0b8112fb0b2cf3d61cb6aeab9627994428fd205c
Abram Symons <abramsymons@gmail.com>
2020-06-30 10:44:57 +0430

    add view to verifications and history

--- v4/IBrightID.sol
+++ v4/IBrightID.sol
@@ -2,6 +2,6 @@ pragma solidity ^0.6.3;
 
 interface IBrightID {
     event Verified(address indexed addr);
-    function verifications(address addr) external returns (uint);
-    function history(address addr) external returns (address);
+    function verifications(address addr) external view returns (uint);
+    function history(address addr) external view returns (address);
 }
